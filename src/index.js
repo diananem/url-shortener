986ebce3815b1b926dbe6f4312e8da45b4c7e117
diananem <diana.nemyrovska@gmail.com>
2018-08-17 21:17:23 +0300

    Set up Apollo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  link: new HttpLink(
+    "https://api.graph.cool/simple/v1/cjky7pfnm593o0196yq9mgr3v"
+  ),
+  cache: new InMemoryCache()
+});
+
+const withApolloProvider = Comp => (
+  <ApolloProvider client={client}>{Comp}</ApolloProvider>
+);
+ReactDOM.render(withApolloProvider(<App />), document.getElementById("root"));
 registerServiceWorker();
