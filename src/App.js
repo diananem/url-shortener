8c27a4a0051eff6d2a95f85b3c8207ed8a0973a1
diananem <diana.nemyrovska@gmail.com>
2018-09-05 19:31:35 +0300

    Added creating short links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import LinkList from "./components/LinkList";
+import CreateShortLink from "./components/CreateShortLink";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LinkList />
+        <div>
+          <h2>All links</h2>
+          <LinkList />
+        </div>
+        <div>
+          <h2>Create a short link</h2>
+          <CreateShortLink />
+        </div>
       </div>
     );
   }
