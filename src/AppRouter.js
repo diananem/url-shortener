5edb0d612c85921fc2dd721198cce8bce964e984
diananem <diana.nemyrovska@gmail.com>
2018-09-11 08:40:26 +0300

    Implement user authentication

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -2,11 +2,15 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from "./App.js";
 import ShortLinkRedirect from "./components/ShortLinkRedirect";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 
 const AppRouter = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={App} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={SignUp} />
       <Route
         path="/:hash"
         render={props => <ShortLinkRedirect hash={props.match.params.hash} />}
